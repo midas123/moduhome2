b28c96aa58408753682bac49ea4be7924ee7e575
midas123 <midasyk123@naver.com>
2019-01-18 18:12:06 +0900

    test 클래스 수정

--- src/test/java/com/kh/cart/CartServiceImplTest.java
+++ src/test/java/com/kh/cart/CartServiceImplTest.java
@@ -7,12 +7,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import org.junit.Test;
 
 class CartServiceImplTest {
-	
 	@Test
-	void testmakeCartInventory() throws Exception {
+	public void testmakeCartInventory() throws Exception {
 		
 		 /*이미 존재하는 세션 장바구니(BeforesessionCart)에는 2개의 상품이 있습니다. 
 		 *새로 추가되는 상품 3개중 2개와 중복 됩니다. 
@@ -61,9 +61,13 @@ class CartServiceImplTest {
 	     
 	     //비회원 장바구니 담기(3개 상품)
 	     Map<String, Object> NotMemberGoods = new HashMap<>();
-	     NotMemberGoods.put("optno[]", new String[] {"1", "2", "3"}); //선택 옵션 인덱스
-	     NotMemberGoods.put("ea[]", new String[] {"2", "4", "1"});//상품 수량
-	     NotMemberGoods.put("kinds[]", new String[] {"344", "345", "350" });//상품 종류 번호
+	     String[] opt = {"1", "2", "3"};
+	     String[] ea = {"2", "4", "1"};
+	     String[] kinds = {"344", "345", "350"};
+	     
+	     NotMemberGoods.put("optno[]", opt); //선택 옵션 인덱스
+	     NotMemberGoods.put("ea[]", ea);//상품 수량
+	     NotMemberGoods.put("kinds[]",kinds );//상품 종류 번호
 	     NotMemberGoods.put("goodsno", "500"); //상품 번호
 	     NotMemberGoods.put("cartSession", BeforesessionCart); //장바구니 담기 기능 실행 전 세션 카트(컨트롤러에서 map 객채에 저장합니다.)
 		CartServiceImpl arrayCheck = new CartServiceImpl();
@@ -71,7 +75,7 @@ class CartServiceImplTest {
 		
 		
 		 //회원 장바구니 담기
-		 Map<String, Object> MemberGood = new HashMap<>();
+/*		 Map<String, Object> MemberGood = new HashMap<>();
 	     MemberGood.put("optno[]", "1");
 	     MemberGood.put("ea[]", "1");
 	     MemberGood.put("kinds[]", "550");
@@ -83,7 +87,7 @@ class CartServiceImplTest {
 	     MemberGoods.put("ea[]", new String[] {"2", "5"});
 	     MemberGoods.put("kinds[]", new String[] {"400", "401"});
 	     MemberGoods.put("goodsno", "600");
-	     MemberGoods.put("MEMBER_NUMBER", 1);
+	     MemberGoods.put("MEMBER_NUMBER", 1);*/
 		
 		
 		
